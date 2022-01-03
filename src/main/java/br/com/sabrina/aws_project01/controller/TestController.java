e6f1eb8bb7e3880397b74a4fa079d83b341852f6
Sabrina Karen <sabrina.souza@elogroup.com.br>
2022-01-03 16:45:18 -0300

    Subida inicial

---
+++ src/main/java/br/com/sabrina/aws_project01/controller/TestController.java
@@ -0,0 +1,23 @@
+package br.com.sabrina.aws_project01.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TestController.class);
+
+    @GetMapping("/dog/{name}")
+    public ResponseEntity<?> dogTest(@PathVariable String name) {
+        LOG.info("Test controller - name: {}", name);
+        return ResponseEntity.ok("Name: " + name);
+    }
+    
+}
---
+++ src/test/java/br/com/sabrina/aws_project01/AwsProject01ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sabrina.aws_project01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsProject01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
